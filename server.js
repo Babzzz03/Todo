68699ba2bff884ee2e900c1f0757ac1e3a7d9723
BABZZ <babawale.emmanuel10@gmail.com>
2022-06-18 10:18:30 -0700

    make it better

---
+++ server.js
@@ -0,0 +1,65 @@
+const connectDB = require('./db/connection')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const tasks = require("./routes/task");
+require('dotenv').config()
+
+const notFound = require('./middleware/notFound');
+const errorHandelerMiddleWare = require ('./middleware/error-handeler');
+const path = require("path");
+
+
+
+
+
+
+
+app.use(express.json());
+
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+
+
+app.get("/hello", (req, res) => {
+  res.send("Task manager app");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+app.use(notFound);
+app.use(errorHandelerMiddleWare);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/todos/build")));
+
+  app.get("*", (request, response) => {
+    response.sendFile(path.join(__dirname, "todos", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Api running");
+  });
+}
+
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try{
+    await connectDB(process.env.MONGO_URI)
+app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch(error) {
+    console.log(error)
+  }
+}
+
+
+
+start()
+
